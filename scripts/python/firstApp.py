2a50fa074e19311f0372aad2a0ab949604af8fdd
aminelli <56305052+aminelli@users.noreply.github.com>
2024-07-11 15:55:47 +0200

    dafa

--- scripts/python/firstApp.py
+++ scripts/python/firstApp.py
@@ -1,7 +1,7 @@
 """SimpleApp.py"""
 from pyspark.sql import SparkSession
 
-logFile = "/tmp//README.md"  # Should be some file on your system
+logFile = "/tmp/README.md"  # Should be some file on your system
 spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
 logData = spark.read.text(logFile).cache()
 
